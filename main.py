f0819397f30b14621366452eb7cff37a64c95b60
VikTheTrick <viktorvujicic1@gmail.com>
2022-12-11 11:49:24 +0100

    1st done

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ centerNum = -1
 averagesArray = []
 centerArray = []
 
+
 def averages(accumulator, current):
     currentIndex = len(accumulator) - 1
     if currentIndex == -1:
@@ -26,11 +27,13 @@ def averages(accumulator, current):
             accumulator.append((current[0], current[2]))
     return accumulator
 
+
 def center(curElement):
     global centerNum
     global averagesArray
     centerNum += 1
-    return curElement[0], curElement[1], curElement[2] - averagesArray[int(centerNum/5068)][1]
+    return curElement[0], curElement[1], curElement[2] - averagesArray[int(centerNum / 5068)][1]
+
 
 def variance(accumulator, current):
     currentIndex = len(accumulator) - 1
@@ -47,21 +50,28 @@ def variance(accumulator, current):
             accumulator[currentIndex] = tuple(tupleList)
             accumulator.append((current[0], current[2]))
     return accumulator
-if __name__ == '__main__':
 
-    #startingData
+
+if __name__ == '__main__':
+    # startingData
 
     df = pd.read_table('ekspresije.tsv', index_col=0)
 
     data = [(cell, gene, value) for cell in df.columns
             for gene, value in df[cell].items()]
 
-    # 1
+    # 1.1
     averagesArray = reduce(averages, data, [])
-    # 2
+    # 1.2
     centerArray = list(map(center, data))
-    # 3
+    centerNum = -1
+    # 1.3
     varianceArray = reduce(variance, centerArray, [])
-    # 4
+    # 1.4
     standardDeviationArray = list(map(lambda x: (x[0], math.sqrt(x[1])), varianceArray))
+    standardDeviationDict = dict(map(lambda x: (x[0], math.sqrt(x[1])), varianceArray))
+    # 1.5
+    standardisedValuesArray = list(map(lambda x: (x[0], x[1], x[2] / standardDeviationDict[x[0]]), centerArray))
+
+    # 2.1
 
